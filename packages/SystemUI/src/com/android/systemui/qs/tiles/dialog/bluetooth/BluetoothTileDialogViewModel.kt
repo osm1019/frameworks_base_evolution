ProjectMatrixx/android_frameworks_base

base: Allow turning on Bluetooth when pressing the tile [1/2]
Respectively
Could save some taps for some users

packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/bluetooth/BluetoothTileDialogViewModel.kt
@@ -78,14 +78,14 @@ constructor(
      * @param view The view from which the dialog is shown.
      */
     @kotlinx.coroutines.ExperimentalCoroutinesApi
-    fun showDialog(context: Context, view: View?) {
+    fun showDialog(context: Context, view: View?, isAutoOn: Boolean = false) {
         cancelJob()
 
         job =
             coroutineScope.launch(mainDispatcher) {
                 var updateDeviceItemJob: Job?
                 var updateDialogUiJob: Job? = null
-                val dialog = createBluetoothTileDialog(context)
+                val dialog = createBluetoothTileDialog(context, isAutoOn)
 
                 view?.let {
                     dialogLaunchAnimator.showFromView(
@@ -164,7 +164,8 @@ constructor(
             }
     }
 
-    private suspend fun createBluetoothTileDialog(context: Context): BluetoothTileDialog {
+    private suspend fun createBluetoothTileDialog(context: Context,
+            isAutoOn: Boolean = false): BluetoothTileDialog {
         val cachedContentHeight =
             withContext(backgroundDispatcher) {
                 sharedPreferences.getInt(
@@ -182,7 +183,8 @@ constructor(
                 systemClock,
                 uiEventLogger,
                 logger,
-                context
+                context,
+                isAutoOn
             )
             .apply { SystemUIDialog.registerDismissListener(this) { cancelJob() } }
     }

osm1019/frameworks_base_evolution

Change-Id: I1c3a2e9b37d7ea07d8d1ba2bee26998bd3c8ac02
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/bluetooth/BluetoothTileDialogViewModel.kt
@@ -82,14 +82,14 @@ constructor(
      * @param view The view from which the dialog is shown.
      */
     @kotlinx.coroutines.ExperimentalCoroutinesApi
-    fun showDialog(context: Context, view: View?) {
+    fun showDialog(context: Context, view: View?, isAutoOn: Boolean = false) {
         cancelJob()
 
         job =
             coroutineScope.launch(mainDispatcher) {
                 var updateDeviceItemJob: Job?
                 var updateDialogUiJob: Job? = null
-                val dialogDelegate = createBluetoothTileDialog(context)
+                val dialogDelegate = createBluetoothTileDialog(context, isAutoOn)
                 val dialog = dialogDelegate.createDialog()
 
                 view?.let {
@@ -206,7 +206,8 @@ constructor(
             }
     }
 
-    private suspend fun createBluetoothTileDialog(context: Context): BluetoothTileDialogDelegate {
+    private suspend fun createBluetoothTileDialog(context: Context,
+            isAutoOn: Boolean = false): BluetoothTileDialogDelegate {
         val cachedContentHeight =
             withContext(backgroundDispatcher) {
                 sharedPreferences.getInt(
@@ -224,7 +225,8 @@ constructor(
             cachedContentHeight,
             bluetoothStateInteractor.isBluetoothEnabled,
             this@BluetoothTileDialogViewModel,
-            { cancelJob() }
+            { cancelJob() },
+            isAutoOn
         )
     }
 